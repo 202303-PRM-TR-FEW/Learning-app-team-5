67f1eec7e7304c1ef7fe92bc34558c9016d86b01
Ceren Şahin <cerennssahinn@gmail.com>
2023-07-24 01:02:41 +0300

    Statistics Page Fixes #6
    Initialized Statistics page
    Modified CourseButton component to accept width percentage as input
    Modified Profile page adding a button to navigate to Statistics page

--- components/course-general-components/CourseButton.jsx
+++ components/course-general-components/CourseButton.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-function CourseButton({ buttonName, handleClick }) {
+function CourseButton({ buttonName, handleClick, widthPercentage }) {
   return (
     <button
-      className="bg-[#56a0fe] w-[45%] rounded-2xl text-white py-2 px-4 hover:bg-[#fbfbfb] hover:text-[#56a0fe] hover:border-[#56a0fe] hover:border-2 text-base"
+      className={`bg-[#56a0fe] w-[${widthPercentage}%] rounded-2xl text-white py-2 px-4 hover:bg-[#fbfbfb] hover:text-[#56a0fe] hover:border-[#56a0fe] hover:border-2 text-base cursor-pointer`}
       onClick={handleClick}
     >
       {buttonName}
     </button>
   );
 }
+
+CourseButton.defaultProps = {
+  buttonName: "Button",
+  handleClick: () => {},
+  widthPercentage: "w-[45%]",
+};
 
 export default CourseButton;
