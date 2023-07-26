fbf2df6014e22fd52405ab9667a522cc32c6be63
wasim altayyan <wasimtayyan@gmail.com>
2023-07-26 12:36:44 +0300

    Feat: moved fetch corses into context file

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,13 +1,16 @@
 import NavBar from "@/components/NavBar/NavBar";
 import { AuthContextProvider } from "../context/AuthContext";
 import { RandomContextProvider } from "../context/RandomNumbers";
+import { CoursesContextProvider } from "../context/FetchAllCourses";
 
 export default function Layout({ children }) {
   return (
     <AuthContextProvider>
       <RandomContextProvider>
-        <NavBar/>
-        {children}
+        <CoursesContextProvider>
+          <NavBar />
+          {children}
+        </CoursesContextProvider>
       </RandomContextProvider>
     </AuthContextProvider>
   );
