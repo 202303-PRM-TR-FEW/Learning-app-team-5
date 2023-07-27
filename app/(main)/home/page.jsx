6b68024bc795a3e4b00e76a928412fceb0b9457a
wasim altayyan <wasimtayyan@gmail.com>
2023-07-27 11:33:49 +0300

    Fix: screen hight

--- app/(main)/home/page.jsx
+++ app/(main)/home/page.jsx
@@ -6,7 +6,7 @@ import FeaturedCourse from "@/components/home-page-components/FeaturedCourse";
 function Page() {
   return (
     <div
-      className="container mx-auto px-4 
+      className="container mx-auto px-4 min-h-screen
     max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-[1370px] 3xl:w-[1650px] dark:text-bodyWhite"
     >
       <FeaturedCourse />
--- app/(main)/profile/page.jsx
+++ app/(main)/profile/page.jsx
@@ -4,7 +4,7 @@ import InfoComp from "@/components/profilePage-components/InfoSide";
 function Profile() {
   return (
     <div
-      className="flex flex-col container mx-auto px-4  md:flex-row gap-20 
+      className="flex flex-col container mx-auto px-4 min-h-screen md:flex-row gap-20 
      max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-[1350px] text-lightBlack dark:text-bodyWhite"
     >
       <InfoComp />
