25ef09e5c392ce9aabfc9ecc1491649811d1cf0e
Maral <maral.mamedovna@gmail.com>
2023-08-06 19:36:53 +0300

    Search Page test components

---
+++ components/search-page-components/RatingFilter.cy.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RatingFilter from './RatingFilter'
+
+describe('<RatingFilter />', () => {
+  it('mounts', () => {
+    cy.mount(<RatingFilter />);
+  })
+
+
+
+})
+
+
