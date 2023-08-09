34fa09f587dfb579e02ecd7a42530950faf8c605
wasim altayyan <wasimtayyan@gmail.com>
2023-08-09 12:01:45 +0300

    Fix: Review function

--- components/course-general-components/CourseOverview.jsx
+++ components/course-general-components/CourseOverview.jsx
@@ -1,6 +1,7 @@
 "use client";
 import dynamic from "next/dynamic";
 import { useState, useEffect } from "react";
+import { useTranslations } from "next-intl";
 const AccessTimeIcon = dynamic(() => import("@mui/icons-material/AccessTime"));
 const StarBorderIcon = dynamic(() => import("@mui/icons-material/StarBorder"));
 const ArrowBackIcon = dynamic(() => import("@mui/icons-material/ArrowBack"));
@@ -19,8 +20,8 @@ const LeaderboardOutlinedIcon = dynamic(() =>
 
 import "./CourseCard.css";
 import CourseButton from "./CourseButton";
-import Image from "next/image";
-import { useTranslations } from "next-intl";
+import RatingStars from "../Review-components/RatingStars";
+import DropUpMenu from "../Review-components/DropUpMenu";
 
 function CourseOverview({ selectedCourse }) {
   const [showArrowIcons, setShowArrowIcons] = useState(false);
@@ -39,7 +40,9 @@ function CourseOverview({ selectedCourse }) {
       <div className="flex flex-col bg-[rgba(251,_251,_251,_0.5)] dark:bg-indigoDay p-4 m-4 rounded-2xl shadow-md justify-center items-center ">
         <div className="flex w-full justify-center items-center">
           <WavingHandOutlinedIcon className="text-[#56a0fe] text-6xl h-1/2 mr-2" />
-          <h1 className="text-2xl font-bold text-[#56a0fe]">{t("Message-1")}</h1>
+          <h1 className="text-2xl font-bold text-[#56a0fe]">
+            {t("Message-1")}
+          </h1>
         </div>
         {showArrowIcons && (
           <div className="flex w-full justify-center items-center">
@@ -59,7 +62,6 @@ function CourseOverview({ selectedCourse }) {
         <div
           className="hidden sm:block h-[300px] p-4 rounded-2xl w-full lg:w-full"
           style={{
-
             backgroundImage: `url(${selectedCourse.courseImage})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
@@ -141,14 +143,18 @@ function CourseOverview({ selectedCourse }) {
 
       {/* PREVIEW AND ENROLL BUTTONS */}
       <div className="flex justify-between my-4">
-        <CourseButton
-          buttonName={selectedCourse.isRegistered ? t("Button-1") : t("Button-2")}
-          handleClick={() =>
-            console.log("add preview page navigation to handleClick function")
+        <DropUpMenu
+          buttonName={
+            selectedCourse.isRegistered ? t("Button-1") : t("Button-2")
           }
-        />
+          courseID={selectedCourse.docID}
+        >
+          <RatingStars />
+        </DropUpMenu>
         <CourseButton
-          buttonName={selectedCourse.isRegistered ? t("Button-3"): t("Button-4")}
+          buttonName={
+            selectedCourse.isRegistered ? t("Button-3") : t("Button-4")
+          }
           handleClick={() =>
             console.log("add enroll page navigation to handleClick function")
           }
